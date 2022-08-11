c656bdca5b491463d726502c9260f507cc769963
Parsa Parishan <pparsaparishan@yahoo.com>
2022-08-11 16:06:14 -0400

    fix bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 export default function App() {
   return (
     <CartContext>
-      <Router basename=" https://parsa-parishan.github.io/My-shop/">
+      <Router basename="https://parsa-parishan.github.io/My-shop/">
         <AnimatedRoutes />
       </Router>
     </CartContext>
